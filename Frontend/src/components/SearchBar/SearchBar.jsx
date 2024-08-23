8be9340897c54b8aff6133a143c29bd814f5614e
Rohith3681 <160815413+Rohith3681@users.noreply.github.com>
2024-08-23 22:32:25 +0530

    search bar

--- Frontend/src/components/SearchBar/SearchBar.jsx
+++ Frontend/src/components/SearchBar/SearchBar.jsx
@@ -5,27 +5,39 @@ export const SearchBar = () => {
   return (
     <div className="search-bar">
       <div className="input-wrapper">
-        <span className="icon">&#128205;</span>
-        <textarea 
-          className="search-input" 
-          placeholder="Location"
-        />
+        <span className="icon"><i className="ri-map-pin-range-line"></i></span>
+        <div className="input-content">
+          <label className="input-label">Location</label>
+          <input 
+            className="search-input" 
+            type="text" 
+            placeholder="Where are you going?" 
+          />
+        </div>
       </div>
       <div className="input-wrapper">
-        <span className="icon">&#128207;</span>
-        <textarea 
-          className="search-input" 
-          placeholder="Distance"
-        />
+        <span className="icon"><i className="ri-pin-distance-line"></i></span>
+        <div className="input-content">
+          <label className="input-label">Distance</label>
+          <input 
+            className="search-input" 
+            type="text" 
+            placeholder="565" 
+          />
+        </div>
       </div>
       <div className="input-wrapper">
-        <span className="icon">&#128101;</span>
-        <textarea 
-          className="search-input" 
-          placeholder="Max People"
-        />
+        <span className="icon"><i className="ri-group-line"></i></span>
+        <div className="input-content">
+          <label className="input-label">Max People</label>
+          <input 
+            className="search-input" 
+            type="text" 
+            placeholder="65656" 
+          />
+        </div>
       </div>
-      <button className="search-button">Search</button>
+      <button className="search-button"><i className="ri-search-line"></i></button>
     </div>
   );
 }
