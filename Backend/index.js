bf3bfc91783c5b7d98a40cfcc13a02639faa2dac
Rohith3681 <160815413+Rohith3681@users.noreply.github.com>
2025-05-05 02:26:31 +0530

    Final

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json({limit:'50mb'}));
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend origin
+    origin: 'https://fdfed-gilt.vercel.app/', // Your frontend origin
     credentials: true
 }));
 app.use(express.json());
